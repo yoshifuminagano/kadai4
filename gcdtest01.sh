b71629526dce7754db82f74981548bfb0e1ebae2
nagano@aiit <b18z2yn@aiit.ac.jp>
2020-07-05 21:31:45 +0900

    Added testcase for verification of gcdcalc01.sh

--- gcdtest01.sh
+++ gcdtest01.sh
@@ -18,5 +18,23 @@ echo 45  と 135  の最大公約数は  45  です. > /tmp/ans-$$
 
 diff /tmp/result-$$ /tmp/ans-$$ || exit 1
 
+./gcdcalc01.sh 4525 27150 > /tmp/result-$$
+
+echo 4525  と 27150  の最大公約数は  4525  です. > /tmp/ans-$$
+
+diff /tmp/result-$$ /tmp/ans-$$ || exit 1
+
+./gcdcalc01.sh 44320 52353 > /tmp/result-$$
+
+echo 44320  と 52353  の最大公約数は  277  です. > /tmp/ans-$$
+
+diff /tmp/result-$$ /tmp/ans-$$ || exit 1
+
+./gcdcalc01.sh 1058 1288 > /tmp/result-$$
+
+echo 1058  と 1288  の最大公約数は  47  です. > /tmp/ans-$$
+
+diff /tmp/result-$$ /tmp/ans-$$ || exit 1
+
 echo "complete"
 
